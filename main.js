edcd264625621d9d4bc0ada0efc9b58e4e5aa224
neel <neelbarvaliya04@gmail.com>
2024-02-21 16:23:43 +0530

    made the js code for the hover effect of the blog preview card

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const blogHeadingEl = document.querySelector(".blog--heading");
+const blogContainerEl = document.querySelector(".blog-card--container");
+
+blogHeadingEl.addEventListener("mouseover", function (e) {
+  blogHeadingEl.style.color = "hsl(47, 88%, 63%)";
+  blogContainerEl.classList.add("blog--active");
+});
+
+blogHeadingEl.addEventListener("mouseout", function (e) {
+  blogHeadingEl.style.color = "hsl(0, 0%, 7%)";
+  blogContainerEl.classList.remove("blog--active");
+});
